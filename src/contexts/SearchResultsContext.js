89c3e1cfc1af693f07a9628a60f6f6d66d1d37e3
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-10-22 03:08:20 +0530

    search results context setup

---
+++ src/contexts/SearchResultsContext.js
@@ -0,0 +1,39 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const SearchResultsContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const SearchResultsProvider = ({ children }) => {
+  //# states
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchItem, setSearchItem] = useState('');
+
+  //# FUNCTIONS
+  const fetchResults = async (resultType) => {
+    setIsLoading(true);
+    const result = await fetch(`${baseUrl}${resultType}`, {
+      method: 'GET',
+      headers: {
+        'x-user-agent': 'desktop',
+        'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+        'x-rapidapi-key': 'SIGN-UP-FOR-KEY',
+      },
+    });
+    const data = await result.json();
+    setResults(data);
+    setIsLoading(false);
+  };
+
+  // # RETURN
+  return (
+    <SearchResultsContext.Provider
+      value={{ fetchResults, results, isLoading, searchItem, setSearchItem }}
+    >
+      {children}
+    </SearchResultsContext.Provider>
+  );
+};
+
+// create custom global hook
+export const useSearchResultsContext = () => useContext(SearchResultsContext);
