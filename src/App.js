06292e73831ae757ae82ea32f060969f1c84ece1
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-10-21 23:59:49 +0530

    first line of talwind css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import { Navbar, Routes, Footer } from './components';
 
 const App = () => {
+  // states
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  // jsx
   return (
-    <main>
-      <Navbar />
-      <Routes />
-      <Footer />
+    <main className={darkTheme ? 'dark' : ''}>
+      <div className="bg-gray-100 dark:bg-gray-900 dg:text-gray-200 min-h-screen">
+        <Navbar />
+        <Routes />
+        <Footer />
+      </div>
     </main>
   );
 };
